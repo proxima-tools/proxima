fc9db2a53eabd82fcea3ca1bcc9eaa695f5f7380
Istvan David <goerenh@gmail.com>
2017-01-26 18:19:30 +0100

    Added DSE toolbar menu item

---
+++ plugins/be.uantwerpen.msdl.icm.tooling.ui/src/be/uantwerpen/msdl/icm/tooling/ui/commands/DSEHandler.java
@@ -0,0 +1,15 @@
+package be.uantwerpen.msdl.icm.tooling.ui.commands;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class DSEHandler extends org.eclipse.core.commands.AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.out.println("Execute DSE!");
+		
+		return null;
+	}
+	
+}
