717446efab6fa0286079fac1270cb1e9c89ee727
Istvan David <goerenh@gmail.com>
2017-04-08 12:07:39 +0200

    #39

---
+++ plugins/be.uantwerpen.msdl.icm.tooling.ui/src/be/uantwerpen/msdl/icm/tooling/ui/commands/ExecutionHandler.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2016-2017 Istvan David
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Istvan David - initial API and implementation
+ *******************************************************************************/
+
+package be.uantwerpen.msdl.icm.tooling.ui.commands;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+/**
+ * @author Istvan David
+ *
+ */
+public class ExecutionHandler extends org.eclipse.core.commands.AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        System.out.println("MOCK execution running");
+        return null;
+    }
+
+}
