a7c53679ca2101fc01d10a7974b2233506688add
Istvan David <goerenh@gmail.com>
2017-04-19 15:42:55 +0200

    #55

--- plugins/be.uantwerpen.msdl.icm.tooling.ui/src/be/uantwerpen/msdl/icm/tooling/ui/wizards/NewProcessModelWizard.java
+++ plugins/be.uantwerpen.msdl.icm.tooling.ui/src/be/uantwerpen/msdl/icm/tooling/ui/wizards/NewProcessModelWizard.java
@@ -11,23 +11,15 @@
 
 package be.uantwerpen.msdl.icm.tooling.ui.wizards;
 
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
-import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
 
-public class NewProcessModelWizard extends Wizard implements INewWizard {
+public class NewProcessModelWizard extends BasicNewResourceWizard implements INewWizard {
 
-    @Override
-    public void init(IWorkbench workbench, IStructuredSelection selection) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public boolean performFinish() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
