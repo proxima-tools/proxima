3a938c664ee1b6c7c1292104a4af78274a0e6742
Istvan David <goerenh@gmail.com>
2017-04-21 13:01:01 +0200

    #60

---
+++ tests/be.uantwerpen.msdl.icm.tests.base/src/be/uantwerpen/msdl/icm/tests/base/ProcessFileBasedTest.java
@@ -0,0 +1,37 @@
+package be.uantwerpen.msdl.icm.tests.base;
+
+import java.io.File;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+import be.uantwerpen.msdl.processmodel.ProcessModel;
+
+public abstract class ProcessFileBasedTest {
+
+	private ProcessModel processModel;
+	private File processModelFile;
+
+	public ProcessFileBasedTest() {
+		processModelFile = new File("processes\\attributetest.processmodel");
+
+		Map<String, Object> extensionToFactoryMap = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap();
+		extensionToFactoryMap.put("processmodel", new XMIResourceFactoryImpl());
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource resource = resourceSet.getResource(URI.createURI(processModelFile.getPath()), true);
+
+		processModel = (ProcessModel) resource.getContents().get(0);
+	}
+
+	public ProcessModel getProcessModel() {
+		return processModel;
+	}
+
+	public File getProcessModelFile() {
+		return processModelFile;
+	}
+}
